d358d18ae314702c99cf11871c16736d9fa8b40a
Sari21 <47698542+Sari21@users.noreply.github.com>
2020-01-27 13:58:53 +0100

    video

--- src/components/GalleryBody.vue
+++ src/components/GalleryBody.vue
@@ -2,6 +2,11 @@
   <div id="body" class="page">
     <h1 class="p-4 md-pt-5">Galéria</h1>
     <div class="d-flex flex-column justify-content-center align-items-center col-12 col-md-8">
+      <iframe
+        src="https://drive.google.com/file/d/1YBJcGnnUcpAKKIIrJrjJRqNmThyU5IKn/preview"
+        width="640"
+        height="480"
+      ></iframe>
       <gallery-template
         v-for="gallery in allGallery"
         v-bind:key="gallery.id"
